4adcc8e14a1c424b1fe183732fee74d0f4a97b5d
Matheus Alexandre <mathe.system@gmail.com>
2020-05-18 00:05:22 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-accout.spec.ts
+++ src/data/usecases/add-account/db-add-accout.spec.ts
@@ -106,4 +106,21 @@ describe('DbAddAccout UserCase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
