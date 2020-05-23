c9563dece902f6c21163811cd24245b6a8c70d28
Matheus Alexandre <mathe.system@gmail.com>
2020-05-23 11:35:24 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -8,7 +8,12 @@ export const MongoHelper = {
       useUnifiedTopology: true
     })
   },
+
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
