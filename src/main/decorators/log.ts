04f482ba029f0a5a63a4daec630f9c648be993b5
Matheus Alexandre <mathe.system@gmail.com>
2020-05-27 19:14:08 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -9,14 +9,6 @@ export class LogControllerDecorator implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     await this.controller.handle(httpRequest)
-    // const httpResponse = await this.controller.handle(httpRequest)
-    //
-    // // if (httpResponse.statusCode === 500) {
-    // //   //   log
-    // // }
-    //
-    // return httpResponse
-
     return null
   }
 }
