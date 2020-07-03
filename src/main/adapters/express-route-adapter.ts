f1b81aa4d9e7e1de9ed6d187f512bc8ceb5913b7
Matheus Alexandre <mathe.system@gmail.com>
2020-07-02 21:01:26 -0300

    fix: add 'else' in ExpressAdapter

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -9,7 +9,8 @@ export const adaptRoute = (controller: Controller) => {
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
     }
-    res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
   }
 }
