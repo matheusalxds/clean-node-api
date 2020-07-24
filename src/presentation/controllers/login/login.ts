737944328a81f35a1e72168af0a76c1f6985dfe6
Matheus Alexandre <mathe.system@gmail.com>
2020-07-23 22:42:04 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { EmailValidator } from '../../protocols/email-validator'
 
@@ -11,19 +11,29 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
+    try {
+      const { email, password } = httpRequest.body
 
-    if (!email) {
-      return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
-    }
+      if (!email) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('email')))
+        )
+      }
 
-    if (!password) {
-      return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
-    }
+      if (!password) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('password')))
+        )
+      }
 
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))))
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return new Promise(resolve =>
+          resolve(badRequest(new InvalidParamError('email')))
+        )
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
