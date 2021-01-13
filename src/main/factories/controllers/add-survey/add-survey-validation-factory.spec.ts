71f582c52e1c8fe93e85a594d843b52c7fb6209d
Matheus Alexandre <mathe.system@gmail.com>
2021-01-12 22:56:50 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+describe('AddSurveyValidation Factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -2,8 +2,8 @@ import { SignUpController } from '../../../../presentation/controllers/login/sig
 import { Controller } from '../../../../presentation/protocols'
 import { makeSignValidation } from './signup-validation-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
-import { makeDbAddAccount } from '../../usecases/db-addcount/db-account-factory'
-import { makeLogControllerDecorator } from '../../usecases/decorators/log-controlelr-decorator-factory'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controlelr-decorator-factory'
 
 export const makeSignUpController = (): Controller => {
   const controller = new SignUpController(makeDbAddAccount(), makeSignValidation(), makeDbAuthentication())
