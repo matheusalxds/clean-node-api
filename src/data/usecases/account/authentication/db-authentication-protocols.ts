00f7f806ffda30958e19517d9e3bae98c6b060a7
Matheus Alexandre <mathe.system@gmail.com>
2021-04-24 19:48:23 -0300

    test: ensure SaveSurveyController calls LoadSurveyById with correct values

--- src/data/usecases/account/authentication/db-authentication-protocols.ts
+++ src/data/usecases/account/authentication/db-authentication-protocols.ts
@@ -1,4 +1,4 @@
-export * from '@/domain/models/account'
+export * from '@/domain/models/account/account'
 export * from '@/domain/usecases/account/authentication'
 export * from '@/data/protocols/db/account/load-account-by-email-repository'
 export * from '@/data/protocols/critography/hash-comparer'
---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -0,0 +1,52 @@
+import { HttpRequest, LoadSurveyById, SurveyModel } from './save-survey-result-controller-protocols'
+import { SaveSurveyResultController } from './save-survey-result-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_survey_id'
+  }
+})
+
+const makeLoadSurveyById = (): LoadSurveyById => {
+  class LoadSurveyByIdStub implements LoadSurveyById {
+    async loadById (id: string): Promise<SurveyModel> {
+      return new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+
+  return new LoadSurveyByIdStub()
+}
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+})
+
+type SutTypes = {
+  sut: SaveSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = makeLoadSurveyById()
+  const sut = new SaveSurveyResultController(loadSurveyByIdStub)
+
+  return {
+    sut,
+    loadSurveyByIdStub
+  }
+}
+
+describe('SaveSurveyResult Controller', () => {
+  test('should call LoadSurveyById with correct values', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(makeFakeRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+})
