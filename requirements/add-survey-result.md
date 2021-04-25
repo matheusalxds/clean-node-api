891bb6327c697759cefeb5f45ac814fb45b60845
Matheus Alexandre <mathe.system@gmail.com>
2021-04-25 10:41:33 -0300

    feat: ensure save survey result route returns 200 on success

--- requirements/add-survey-result.md
+++ requirements/add-survey-result.md
@@ -3,8 +3,8 @@
 
 > ## Caso de sucesso:
 
-1. ⛔️ Recebe uma requisição do tipo **PUT** na rota **/api/surveys/{survey_id}/results**
-1. ⛔️ Valida se a requisição foi feita por um **usuário**
+1. ✅ Recebe uma requisição do tipo **PUT** na rota **/api/surveys/{survey_id}/results**
+1. ✅ Valida se a requisição foi feita por um **usuário**
 1. ✅ Valida dados o parâmentro **surveyy_id**
 1. ✅ Valida se o campo **answer** é uma resposta válida
 1. ✅ **Cria** um resultado de enquete com os dados fornecidos caso não tenha um registro
@@ -13,8 +13,8 @@
 
 > ## Exceções
 
-1. ⛔️ Retorna erro **404** se a API não existir
-1. ⛔️ Retorna erro **403** se não for um usuário
+1. ✅ Retorna erro **404** se a API não existir
+1. ✅ Retorna erro **403** se não for um usuário
 1. ✅ Retorna erro **403** se o survey_id passado na URL for inválido
 1. ✅ Retorna erro **403** se a resposta enviada pelo cliente for uma resposta inválidas
 1. ✅️ Retorna erro **500** se der erro ao tentar criar o resultado da enquete
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,10 @@
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
 import { AddSurveyModel } from '@/domain/usecases/survey/add-survey'
-import { SurveyModel } from '@/domain/models/survey'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+import { SurveyModel } from '@/domain/models/survey/survey'
+import { ObjectId } from 'mongodb'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
@@ -19,7 +20,7 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
 
   async loadById (id: string): Promise<SurveyModel> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const survey = await surveyCollection.findOne({ _id: id })
+    const survey = await surveyCollection.findOne({ _id: new ObjectId(id) })
     return survey && MongoHelper.map(survey)
   }
 }
