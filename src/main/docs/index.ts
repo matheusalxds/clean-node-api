7401df6914669c185e5b1ef3c4f085624c9da426
Matheus Alexandre <mathe.system@gmail.com>
2021-05-11 23:32:05 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from '@/main/docs/paths/login-path'
+import { accountSchema } from '@/main/docs/schemas/account-schema'
+import { loginParamsSchema } from '@/main/docs/schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'Mango\'s course to create surveys between programmers',
     version: '1.0.0'
+  },
+  servers: [
+    { url: '/api' }
+  ],
+  tags: [
+    { name: 'Login' }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,27 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API to user\'s authentication',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
