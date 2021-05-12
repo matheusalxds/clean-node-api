4f4fc72118295a5bb6c455846c6b3593ffba58e1
Matheus Alexandre <mathe.system@gmail.com>
2021-05-11 22:41:09 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,12 @@
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupSwaggr from './config-swagger'
 import express from 'express'
 
 const app = express()
+
+// docs
+setupSwaggr(app)
 
 // middlewares
 setupMiddlewares(app)
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Mango\'s course to create surveys between programmers',
+    version: '1.0.0'
+  }
+}
