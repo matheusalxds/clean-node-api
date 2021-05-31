88d81fd99cca33a1883d45314e981a4318159c00
Matheus Alexandre <mathe.system@gmail.com>
2021-05-30 21:05:16 -0300

    test: ensure LoadSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/survey-result/load-survey-result/load-survey-result-controllelr.spec.ts
+++ src/presentation/controllers/survey-result/survey-result/load-survey-result/load-survey-result-controllelr.spec.ts
@@ -2,8 +2,8 @@ import { LoadSurveyResultController } from './load-survey-result-controller'
 import { HttpRequest, LoadSurveyById, LoadSurveyResult } from './load-survey-result-controller-protocols'
 import { InvalidParamError } from '@/presentation/errors'
 import { mockLoadSurveyById } from '@/presentation/test'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
-import { mockLoadSurveyResult, throwError } from '@/domain/test'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { mockLoadSurveyResult, mockSurveyResultModel, throwError } from '@/domain/test'
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -62,5 +62,11 @@ describe('LoadSurveyResult Controller', () => {
     jest.spyOn(loadSurveyResultStub, 'load').mockImplementationOnce(throwError)
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(mockSurveyResultModel()))
   })
 })
