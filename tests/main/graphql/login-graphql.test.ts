667574273caca8c5d63ad189db8e68da915bc28d
Matheus <mathe.system@gmail.com>
2022-03-09 17:34:23 -0300

    test: ensure Login Query returns UnauthorizedError on invalid credentials

--- tests/main/graphql/login-graphql.test.ts
+++ tests/main/graphql/login-graphql.test.ts
@@ -32,7 +32,7 @@ describe('Login GraphQL', () => {
       }
     }`
 
-    test('Should return an Account on valid credentials', async () => {
+    test('should return an Account on valid credentials', async () => {
       const password = await hash('123', 12)
       await accountCollection.insertOne({
         name: 'Matheus',
@@ -45,6 +45,15 @@ describe('Login GraphQL', () => {
       expect(res.status).toBe(200)
       expect(res.body.data.login.accessToken).toBeTruthy()
       expect(res.body.data.login.name).toBe('Matheus')
+    })
+
+    test('should return UnauthorizedError on invalid credentials', async () => {
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(401)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('Unauthorized')
     })
   })
 })
